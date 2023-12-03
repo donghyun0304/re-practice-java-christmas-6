7f7f091588a699c6d6a7283947f63e9467d02141
donghyunkim <ghffu405@gmail.com>
2023-12-03 16:42:38 +0900

    feat: 에러메시지 추가

--- src/main/java/christmas/domain/ErrorMessage.java
+++ src/main/java/christmas/domain/ErrorMessage.java
@@ -2,6 +2,7 @@ package christmas.domain;
 
 public class ErrorMessage {
 
+    private static final String NOT_VALID_ORDER = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
     public static final String NOT_ALL_NUMERIC = "[ERROR] 숫자로만 이루어져 있지 않습니다.";
     public static final String NOT_VALID_DATE = "[ERROR] 날짜가 유효하지 않습니다.";
 
