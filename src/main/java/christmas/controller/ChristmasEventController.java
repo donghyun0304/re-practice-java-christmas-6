e76a98968f900557c095d44fc21dcabcf5c2c49e
donghyunkim <ghffu405@gmail.com>
2023-12-03 16:13:08 +0900

    feat: 시작 출력 메소드 구현

--- src/main/java/christmas/controller/ChristmasEventController.java
+++ src/main/java/christmas/controller/ChristmasEventController.java
@@ -1,4 +1,19 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class ChristmasEventController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ChristmasEventController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run(){
+
+    }
 }
