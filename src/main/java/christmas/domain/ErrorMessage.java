594d9afc7b8cf8057959b1c51afdfb4d7aca7432
donghyunkim <ghffu405@gmail.com>
2023-12-03 17:26:01 +0900

    feat: 입려형식 에러메시지 추가

--- src/main/java/christmas/domain/ErrorMessage.java
+++ src/main/java/christmas/domain/ErrorMessage.java
@@ -6,5 +6,6 @@ public class ErrorMessage {
     public static final String NOT_ALL_NUMERIC = "[ERROR] 숫자로만 이루어져 있지 않습니다.";
     public static final String NOT_VALID_DATE = "[ERROR] 날짜가 유효하지 않습니다.";
     public static final String NOT_INCLUDE_COMMA = "[ERROR] , 가 포함되어 있지 않습니다.";
+    public static final String NOT_VALID_FORMAT = "[ERROR] 입력 형식이 맞지 않습니다.";
 
 }
