013f321f6227f1c11fea78c0f6364040bc69bf04
donghyunkim <ghffu405@gmail.com>
2023-12-03 18:30:52 +0900

    refactor: static 및 final 추가

--- src/main/java/christmas/dto/FoodsDto.java
+++ src/main/java/christmas/dto/FoodsDto.java
@@ -11,18 +11,18 @@ public class FoodsDto {
 
     private Map<Food, Integer> foods;
 
-    private FoodsDto(Map<Food, Integer> foods) {
+    private FoodsDto(final Map<Food, Integer> foods) {
         this.foods = foods;
     }
 
-    public FoodsDto from(Map<String, Integer> foods){
+    public static FoodsDto from(final Map<String, Integer> foods){
         return new FoodsDto(parse(foods));
     }
     public Map<Food, Integer> getFoods() {
         return Collections.unmodifiableMap(foods);
     }
 
-    private Map<Food, Integer> parse(Map<String, Integer> foods){
+    private static Map<Food, Integer> parse(final Map<String, Integer> foods){
         return foods.entrySet()
                 .stream()
                 .collect(Collectors.toMap(
