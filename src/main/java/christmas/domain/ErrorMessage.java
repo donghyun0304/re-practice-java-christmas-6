ae733ce387aa729fe8c2506283da048855b74391
donghyunkim <ghffu405@gmail.com>
2023-12-03 16:28:25 +0900

    feat: 날짜 에러메시지 추가

--- src/main/java/christmas/domain/ErrorMessage.java
+++ src/main/java/christmas/domain/ErrorMessage.java
@@ -3,5 +3,6 @@ package christmas.domain;
 public class ErrorMessage {
 
     public static final String NOT_ALL_NUMERIC = "[ERROR] 숫자로만 이루어져 있지 않습니다.";
+    public static final String NOT_VALID_DATE = "[ERROR] 날짜가 유효하지 않습니다.";
 
 }
