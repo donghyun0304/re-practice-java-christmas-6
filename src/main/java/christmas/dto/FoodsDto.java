89185a1e33638f24ad5283abd1684a3de734666b
donghyunkim <ghffu405@gmail.com>
2023-12-03 18:29:40 +0900

    refactor: 정적팩토리 메소드로 리팩토링

--- src/main/java/christmas/dto/FoodsDto.java
+++ src/main/java/christmas/dto/FoodsDto.java
@@ -2,20 +2,24 @@ package christmas.dto;
 
 import christmas.domain.Food;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FoodsDto {
 
-    private Map<String, Integer> foods;
+    private Map<Food, Integer> foods;
 
-    public FoodsDto(Map<String, Integer> foods) {
-        this.foods = new HashMap<>(foods);
+    private FoodsDto(Map<Food, Integer> foods) {
+        this.foods = foods;
     }
 
+    public FoodsDto from(Map<String, Integer> foods){
+        return new FoodsDto(parse(foods));
+    }
     public Map<Food, Integer> getFoods() {
-        return parse(foods);
+        return Collections.unmodifiableMap(foods);
     }
 
     private Map<Food, Integer> parse(Map<String, Integer> foods){
