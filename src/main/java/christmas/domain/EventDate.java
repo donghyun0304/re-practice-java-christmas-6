a498b883a93c4a1e76741dc4b38defc6c15a2851
donghyunkim <ghffu405@gmail.com>
2023-12-03 16:33:47 +0900

    feat: toString 추가

--- src/main/java/christmas/domain/EventDate.java
+++ src/main/java/christmas/domain/EventDate.java
@@ -1,4 +1,33 @@
 package christmas.domain;
 
+import javax.swing.plaf.BorderUIResource;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 public class EventDate {
+
+    private static final int YEAR = 2023;
+    private static final int MONTH = 12;
+
+    private final LocalDate date;
+
+    private EventDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public static EventDate from(int day){
+        try {
+            LocalDate.of(YEAR, MONTH, day)
+        } catch (DateTimeException e){
+            new IllegalArgumentException(ErrorMessage.NOT_VALID_DATE, e);
+        }
+        return new EventDate(LocalDate.of(YEAR, MONTH, day));
+    }
+
+    @Override
+    public String toString() {
+        return "EventDate{" +
+                "date=" + date +
+                '}';
+    }
 }
