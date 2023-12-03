7f7789c7a7eaad9e00aa0c455005a516fa0abf36
donghyunkim <ghffu405@gmail.com>
2023-12-03 17:13:09 +0900

    feat: 구분자로 List로 parse하는 메소드 구현

--- src/main/java/christmas/utils/Converter.java
+++ src/main/java/christmas/utils/Converter.java
@@ -1,8 +1,19 @@
 package christmas.utils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Converter {
 
     public static int parseInteger(String str){
         return Integer.parseInt(str);
     }
+
+    public static List<String> parseToStringListByDelimiter(final String str, final String delimiter){
+        return Arrays.stream(str.split(delimiter))
+                .collect(Collectors.toList());
+    }
+
+
 }
