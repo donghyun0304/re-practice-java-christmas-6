38aaa8764b4131eea9dfd357176af5b61903f5c2
donghyunkim <ghffu405@gmail.com>
2023-12-03 16:19:58 +0900

    feat: 정규식 Enum 추가

---
+++ src/main/java/christmas/domain/RegexPattern.java
@@ -0,0 +1,18 @@
+package christmas.domain;
+
+import java.util.regex.Pattern;
+
+public enum RegexPattern {
+
+    ONLY_NUMBER(Pattern.compile("^[0-9]+$")),
+    NOT_INCLUDE_COMMA(Pattern.compile("^[^,]*$"));
+    private final Pattern pattern;
+
+    RegexPattern(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public boolean matches(String input){
+        return pattern.matcher(input).matches();
+    }
+}
