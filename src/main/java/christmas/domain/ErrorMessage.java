6584f751ac6ac5fbad08af3cbdcfa5cb0b784404
donghyunkim <ghffu405@gmail.com>
2023-12-03 17:51:59 +0900

    feat: 숫자 에러메시지 추가

--- src/main/java/christmas/domain/ErrorMessage.java
+++ src/main/java/christmas/domain/ErrorMessage.java
@@ -7,5 +7,7 @@ public class ErrorMessage {
     public static final String NOT_VALID_DATE = "[ERROR] 날짜가 유효하지 않습니다.";
     public static final String NOT_INCLUDE_COMMA = "[ERROR] , 가 포함되어 있지 않습니다.";
     public static final String NOT_VALID_FORMAT = "[ERROR] 입력 형식이 맞지 않습니다.";
+    public static final String NOT_OVER_ZERO = "[ERROR] 0보다 큰 숫자가 아닙니다.";
+    
 
 }
