e72d9648d69e6ae9a2ee862acbb931e20bbbf4c7
donghyunkim <ghffu405@gmail.com>
2023-12-05 02:20:42 +0900

    feat: 정적팩토리메소드로 객체 생성

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,4 +1,26 @@
 package christmas.domain;
 
+import christmas.domain.discount.Discounts;
+
+import java.util.EnumMap;
+
 public class Order {
+
+    private final Foods foods;
+    private final Discounts discounts;
+
+    private Order(final Foods foods, final Discounts discounts) {
+        this.foods = foods;
+        this.discounts = discounts;
+    }
+
+    public static Order of(final Foods foods, final Discounts discounts){
+        return new Order(foods, discounts);
+    }
+
+    public int calcTotalAmountBeforeDiscounts(){
+        return foods.calcSumOfPricesOfFoods();
+    }
+
+
 }
