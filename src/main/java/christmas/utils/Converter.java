d2b9a4f3c1091055bb3423e96a9b222c9df3fcb3
donghyunkim <ghffu405@gmail.com>
2023-12-05 14:58:08 +0900

    refactor: 타입 매개변수 변경

--- src/main/java/christmas/utils/Converter.java
+++ src/main/java/christmas/utils/Converter.java
@@ -1,7 +1,9 @@
 package christmas.utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Converter {
@@ -15,5 +17,14 @@ public class Converter {
                 .collect(Collectors.toList());
     }
 
+
+    //Map<T, K> 타입을 List<T> 타입으로 변환하는 메소드
+    public static <K, V> List<K> parseToKeyTypeList(Map<K, V> map) {
+        List<K> list = new ArrayList<>();
+        for (K key : map.keySet()) {
+            list.add(key);
+        }
+        return list;
+    }
 
 }
