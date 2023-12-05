c3f264ea49931cf662df954bd628bbc85a1ac2fc
donghyunkim <ghffu405@gmail.com>
2023-12-05 16:30:01 +0900

    refactor: 제네릭 클래스로 변경

--- src/main/java/christmas/domain/Present.java
+++ src/main/java/christmas/domain/Present.java
@@ -1,4 +1,14 @@
 package christmas.domain;
 
-public class Present {
+public class Present<T> {
+
+    private final T present;
+
+    public Present(T present) {
+        this.present = present;
+    }
+
+    public T getPresent() {
+        return present;
+    }
 }
