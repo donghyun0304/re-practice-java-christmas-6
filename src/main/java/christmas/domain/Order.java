382135f780604ca7558c129979c03f445c108b98
donghyunkim <ghffu405@gmail.com>
2023-12-05 14:39:12 +0900

    feat: PresentCondition 의존성 주입 코드 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,10 +1,9 @@
 package christmas.domain;
 
+import christmas.domain.condition.DiscountCondition;
+import christmas.domain.condition.PresentCondition;
 import christmas.domain.discount.*;
-import christmas.utils.Converter;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class Order {
@@ -13,15 +12,19 @@ public class Order {
     private final OrderedFoods foods;
     private final Discounts discounts;
     private final DiscountCondition discountCondition;
+    private final PresentCondition presentCondition;
 
-    private Order(final OrderedFoods foods, final Discounts discounts, final DiscountCondition discountCondition) {
+    private Order(final OrderedFoods foods, final Discounts discounts,
+                  final DiscountCondition discountCondition, final PresentCondition presentCondition) {
         this.foods = foods;
         this.discounts = discounts;
         this.discountCondition = discountCondition;
+        this.presentCondition = presentCondition;
     }
 
-    public static Order of(final OrderedFoods foods, final Discounts discounts, final DiscountCondition discountCondition){
-        return new Order(foods, discounts, discountCondition);
+    public static Order of(final OrderedFoods foods, final Discounts discounts,
+                           final DiscountCondition discountCondition, final PresentCondition presentCondition){
+        return new Order(foods, discounts, discountCondition, presentCondition);
     }
 
     public int calcTotalPriceBeforeDiscounts(){
