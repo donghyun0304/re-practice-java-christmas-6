da82761befa4d06c5dccca3bca829895ec01d2c4
donghyunkim <ghffu405@gmail.com>
2023-12-05 11:12:56 +0900

    feat: 할인조건 구현체 생성

--- src/main/java/christmas/domain/discount/DiscountConditionImpl.java
+++ src/main/java/christmas/domain/discount/DiscountConditionImpl.java
@@ -1,2 +1,24 @@
-package christmas.domain.discount;public class DiscountConditionImpl {
+package christmas.domain.discount;
+
+import christmas.domain.Food;
+import christmas.domain.Menu;
+
+import java.util.List;
+
+public class DiscountConditionImpl implements DiscountCondition{
+
+    private static final int MIN_PRICE = 10_000;
+    @Override
+    public boolean isMoreThanMinPrice(int price) {
+        if(price >= MIN_PRICE){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasOnlyDrinks(List<Food> foods) {
+        return foods.stream()
+                .allMatch(food -> Menu.hasFoodInCategory(Menu.DRINKS, food));
+    }
 }
